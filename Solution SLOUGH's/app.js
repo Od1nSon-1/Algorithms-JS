ca0965d6471cc698bc1fab3c135b8f71891d7cf7
Od1nSon-1 <Costobile@yandex.ru>
2023-11-10 17:55:30 +0300

    Solution SLOUGHs first commit

---
+++ Solution SLOUGH's/app.js
@@ -0,0 +1,14 @@
+const inputAValue1 = document.getElementById('input-a-1')
+const inputAValue2 = document.getElementById('input-a-2')
+const inputAValue3 = document.getElementById('input-a-3')
+const inputAValue4 = document.getElementById('input-a-4')
+const inputAValue5 = document.getElementById('input-a-5')
+const inputAValue6 = document.getElementById('input-a-6')
+const inputAValue7 = document.getElementById('input-a-7')
+const inputAValue8 = document.getElementById('input-a-8')
+const inputAValue9 = document.getElementById('input-a-9')
+
+const inputBValue1 = document.getElementById('input-b-1')
+const inputBValue2 = document.getElementById('input-b-1')
+const inputBValue3 = document.getElementById('input-b-1')
+
